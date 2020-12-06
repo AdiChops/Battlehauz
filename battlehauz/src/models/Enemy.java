c1eb05c9cd7bd6cf16ace392d60b79f00074d197
zaraahlie <71647345+zaraahlie@users.noreply.github.com>
2020-12-06 15:11:03 -0500

    Uncommented out Enemy class

--- battlehauz/src/models/Enemy.java
+++ battlehauz/src/models/Enemy.java
@@ -1,62 +1,62 @@
-//package models;
-//
-//import java.util.Random;
-//abstract class Enemy extends GameCharacter implements interfaces.Battleable {
-//    protected int mana;// will be used later to generate special move
-//
-//    public Enemy(String n, int baseHealth, Move[] moves, int mana) {
-//        super(n, baseHealth, moves);
-//        this.mana = mana;
-//    }
-//
-//
-//    public String generateName(){
-//        //generates name for enemy
-//        return null;
-//    }
-//
-//    public int chooseMove() {
-//        //randomly selects a move from moves array
-//        //returns the index
-//        int upperbound = moves.length;
-//        Random rand = new Random();
-//        int index = rand.nextInt(upperbound);
-//        return index;
-//    }
-//
-//    public boolean attackSuccessful() {
-//        int upperbound = 100;
-//        Random rand = new Random();
-//        int prob = rand.nextInt(upperbound);
-//        if (prob >=5){
-//                return true;
+package models;
+
+import java.util.Random;
+abstract class Enemy extends GameCharacter implements interfaces.Battleable {
+    protected int mana;// will be used later to generate special move
+
+    public Enemy(String n, int baseHealth, Move[] moves, int mana) {
+        super(n, baseHealth, moves);
+        this.mana = mana;
+    }
+
+
+    public String generateName(){
+        //generates name for enemy
+        return null;
+    }
+
+    public int chooseMove() {
+        //randomly selects a move from moves array
+        //returns the index
+        int upperbound = moves.length;
+        Random rand = new Random();
+        int index = rand.nextInt(upperbound);
+        return index;
+    }
+
+    public boolean attackSuccessful() {
+        int upperbound = 100;
+        Random rand = new Random();
+        int prob = rand.nextInt(upperbound);
+        if (prob >=5){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        }
+
+//    public void GiveDamage(enemyMove) {
+//        if (AttackSuccessful()){
+//            if (Player.getBaseHealth() >= enemyMove.getDamage()) {
+//                Player.getBaseHealth() -= enemyMove.getDamage();
+//            } else {
+//                dies();
 //            }
-//            else{
-//                return false;
-//            }
-//
-//        }
-//
-////    public void GiveDamage(enemyMove) {
-////        if (AttackSuccessful()){
-////            if (Player.getBaseHealth() >= enemyMove.getDamage()) {
-////                Player.getBaseHealth() -= enemyMove.getDamage();
-////            } else {
-////                dies();
-////            }
-////        }
-////
-////    }
-//
-//    public void takeDamage(Move playerMove, Player player) {
-//        //calls dies() if dead.
-//        if (player.attackSuccessful() == true){
-//            if(this.getBaseHealth() >= playerMove.getDamage()){
-//                this.baseHealth -= playerMove.getDamage();
-//            }
-//
 //        }
 //
 //    }
-//    // abstract void draw()  this will be helpful for graphics, different images
-//}
+
+    public void takeDamage(Move playerMove, Player player) {
+        //calls dies() if dead.
+        if (player.attackSuccessful() == true){
+            if(this.getBaseHealth() >= playerMove.getDamage()){
+                this.baseHealth -= playerMove.getDamage();
+            }
+
+        }
+
+    }
+    // abstract void draw()  this will be helpful for graphics, different images
+}
