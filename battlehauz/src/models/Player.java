3b34c171f1eb78fbe989e067b827a0d46715636c
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2020-12-07 01:53:00 -0500

    some player progress

--- battlehauz/src/models/Player.java
+++ battlehauz/src/models/Player.java
@@ -1,7 +1,12 @@
 package models;
 
-public class Player extends GameCharacter{
+import interfaces.Battleable;
+
+import java.util.HashMap;
+
+public class Player extends GameCharacter implements Battleable {
     private int coins;
+    private HashMap<Item, Integer> Items;
 
     public Player(String name, int maxHealth, int xp) {
         super(name, maxHealth, xp);
@@ -9,6 +14,26 @@ public class Player extends GameCharacter{
 
     @Override
     public String toString(){
+        return"Full Summary\n ------------------------------ \n"+
+                this.getName() + "\nYou are level " + this.calculateLevel() + "\n"
+                + this.getXP() + "XP\n"
+                + this.getCurrentHealth() + " health / " + this.getMaxHealth() + "\n"
+                + this.getCoins() + " coins"
+                + "Your available moves are " + this.printMoves();
+    }
 
+    @Override
+    public boolean attackSuccessful() {
+        return false;
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+
+    }
+
+    @Override
+    public Move performTurn() {
+        return null;
     }
 }
