06e1e63aefab073f6a52906e80ed03530c888b46
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2020-12-13 21:26:48 -0500

    Turn class comments added

--- battlehauz/src/models/Turn.java
+++ battlehauz/src/models/Turn.java
@@ -2,12 +2,13 @@ package models;
 
 import models.items.Item;
 
-// Class that is used to return when an opponent performs their turn
+// Class that is used to return when a GameCharacter performs their turn
 public class Turn {
     private final Move usedMove;
     private final boolean success;
     private final Item usedItem;
 
+    //********************[Constructors]*********************
     public Turn(Move m, boolean s) {
         this.usedMove = m;
         this.usedItem = null;
@@ -19,6 +20,8 @@ public class Turn {
         this.usedItem = i;
         this.success = s;
     }
+    //***************[End of Constructors]*******************
+    //*********************[Getters]*************************
 
     public Move getUsedMove() {
         return usedMove;
@@ -32,10 +35,22 @@ public class Turn {
         return success;
     }
 
+    //******************[End of Getters]*********************
+    //*****************[String functions]********************
+
+    /**
+     * This function returns a string representation of the move the character
+     * used and whether or not the move was successful
+     * @return String representation of move used and success
+     */
     public String toStringMove() {
         return "used " + getUsedMove().getName() + ((isSuccess()) ? "" : "\nWhoa! The move didn't hit!");
     }
 
+    /**
+     * This function returns a string representation of the item the character used
+     * @return String representation of item used
+     */
     public String toStringItem() {
         return "used " + getUsedItem().getName() + ". " + getUsedItem();
     }
